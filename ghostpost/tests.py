5b9913fac22427031bec999da6a6b2c27dac9ba5
Mike A <jimalik2525@gmail.com>
2020-05-14 18:38:15 -0400

    Added Notes on citations for work

--- ghostpost/tests.py
+++ ghostpost/tests.py
@@ -7,6 +7,7 @@ Watched demo,
 Asked for help from Matt for understanding the BooleanValue for the Post model,
 Referenced Previously completed assessments,
 https://stackoverflow.com/questions/47994336/django-filter-boolean-fields,
-https://stackoverflow.com/questions/51781841/delete-a-post-in-django
-https://stackoverflow.com/questions/18331591/how-to-add-multiply-number-variables-in-a-django-template
+https://stackoverflow.com/questions/51781841/delete-a-post-in-django,
+https://stackoverflow.com/questions/18331591/how-to-add-multiply-number-variables-in-a-django-template,
+https://youtu.be/nle3u6Ww6Xk
 """
