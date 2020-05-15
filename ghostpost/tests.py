a24b4d58d3af83c229a8140c6dd04d1059dbbd19
Mike A <jimalik2525@gmail.com>
2020-05-15 10:04:10 -0400

    Added last citation for works cited on test.py

--- ghostpost/tests.py
+++ ghostpost/tests.py
@@ -5,6 +5,7 @@
 __author__ = """
 Watched demo,
 Asked for help from Matt for understanding the BooleanValue for the Post model,
+Also Matt helped me with fixing the last part of my vote count,
 Referenced Previously completed assessments,
 https://stackoverflow.com/questions/47994336/django-filter-boolean-fields,
 https://stackoverflow.com/questions/51781841/delete-a-post-in-django,
